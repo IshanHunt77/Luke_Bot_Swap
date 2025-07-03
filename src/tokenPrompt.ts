a8fd2879d81e982973be9b1a94d949000ec840a3
IshanHunt77 <ishanraghuvanshi046@gmail.com>
2025-07-03 20:58:41 +0530

    v4

--- src/tokenPrompt.ts
+++ src/tokenPrompt.ts
@@ -1,14 +1,14 @@
-import { GoogleGenAI } from "@google/genai";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({ apiKey:"AIzaSyCm-_oseO2HwlI_RUiCFHVDHUGcEA_maK0"});
+// const ai = new GoogleGenerativeAI({apiKey:"" });
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Give the name,symbol,address,decimals of the token  on solana in key value format",
-  });
-  console.log(response.text);
-}
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: "Give the name,symbol,address,decimals of the token  on solana in key value format",
+//   });
+//   console.log(response.text);
+// }
 
-main();
+// main();
